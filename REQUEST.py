7f0ef38de2c7934fc9d436d894ecc12d7213f403
ChampionsofEurope <86567033+ChampionsofEurope@users.noreply.github.com>
2021-07-17 17:54:24 +0100

    Update REQUEST.py

--- REQUEST.py
+++ REQUEST.py
@@ -1,3 +1,6 @@
 import requests
+from bs4 import BeautifulSoup
 page = requests.get("https://dataquestio.github.io/web-scraping-pages/simple.html")
 print(page)
+soup = BeautifulSoup(page.content, 'html.parser')
+print(soup.prettify())
