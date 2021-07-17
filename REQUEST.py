e357b8320e6e2ccde9e5c416a8d808ef826d69e3
ChampionsofEurope <86567033+ChampionsofEurope@users.noreply.github.com>
2021-07-17 18:11:01 +0100

    Update REQUEST.py

--- REQUEST.py
+++ REQUEST.py
@@ -1,7 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
-page = requests.get("https://dataquestio.github.io/web-scraping-pages/simple.html")
-print(page)
-soup = BeautifulSoup(page.content, 'html.parser')
-print(soup.prettify())
-list(soup.children)
+from tkinter import *
+
+root = Tk()
+root.geometry("400x400")
+
+
+
+def pagewrap():
+    page = requests.get("https://dataquestio.github.io/web-scraping-pages/simple.html")
+    print(page)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    print(soup.prettify())
+    list(soup.children)
+    [type(item) for item in list(soup.children)]
+    html = list(soup.children)
+b1 = Button(root, command =  pagewrap, width = 22, text = "Press Button for Details")
+b1.pack(side = BOTTOM)
+
+root.mainloop()
